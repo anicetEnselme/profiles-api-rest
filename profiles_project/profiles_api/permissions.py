77b642b54d9eca4d4119b8ce7e7e9ae1796b1f55
Enselme <enselme20@gmail.com>
2020-03-15 02:37:44 +0100

    Added searching to user profile api

--- profiles_project/profiles_api/permissions.py
+++ profiles_project/profiles_api/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-class updateOwnProfile(permissions.BasePermission):
+class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to edit their own profile"""
     
     def has_object_permission(self, request, view, obj):
